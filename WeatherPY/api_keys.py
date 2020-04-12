5316b5308447b204db2893d78ab4a5cfba2caf14
Gregory Shreve <gshreve1966@gmail.com>
2020-04-12 18:54:58 -0400

    At the point the cities.csv file is saved

--- WeatherPY/api_keys.py
+++ WeatherPY/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "83bf3f795eefb281f8549b044cd31cdc"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCYoDvAcVGo8l0Kt7dEZ9EGVPEBUfJa3Uo"
---
+++ WeatherPY/gs_challenge_lib.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from api_keys import weather_api_key
+from citipy import citipy
+from pprint import pprint
+import requests
+
+from weather_lib import Perform_Weather_Check
+
+#TODO: Remove this function as it is pulled from citypy
+# read in the cities .csv file
+# def load_random_cities(sample_size):
+#     world_cities = pd.read_csv("../resources/world-cities.csv")
+
+#     np.random.seed(411)
+#     chosen_idx = np.random.choice(len(world_cities), replace=True, size=sample_size)             
+#     sample_cities = world_cities.iloc[chosen_idx]
+    
+#     return sample_cities
+
+#Returns data frame with loaded cities latitue and logitude
+def Load_Random_Cities_Locations():
+    # List for holding lat_lngs and cities
+    lat_lngs = []
+    cities = []
+    # Create a set of random lat and lng combinations
+    lats = np.random.uniform(low=-90.000, high=90.000, size=1500)
+    lngs = np.random.uniform(low=-180.000, high=180.000, size=1500)
+    lat_lngs = zip(lats, lngs)
+    
+    # Identify nearest city for each lat, lng combination
+    for lat_lng in lat_lngs:
+        city = citipy.nearest_city(lat_lng[0], lat_lng[1]).city_name
+        
+        # If the city is unique, then add it to a our cities list
+        if city not in cities:
+            cities.append(city)
+    
+    # Print the city count to confirm sufficient count
+    print(len(cities))
+    return pd.DataFrame(cities)
+    
+
+cities = Load_Random_Cities_Locations()
+print(cities.head())
+cities_data = Perform_Weather_Check(cities)
+cities_data.to_csv("Output_Data/cities2.csv")
