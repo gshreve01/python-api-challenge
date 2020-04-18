2fb7261e44e72719292875d4f19308a0699bb606
Gregory Shreve <gshreve1966@gmail.com>
2020-04-18 10:34:26 -0400

    removed actual aki key values

--- api_keys.py
+++ api_keys.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "83bf3f795eefb281f8549b044cd31cdc"
+weather_api_key = "your key goes here"
 
 # Google API Key
-g_key = "AIzaSyCYoDvAcVGo8l0Kt7dEZ9EGVPEBUfJa3Uo"
-encrypted_g_key = "5JvARlwhIjnnRNMPpn%2FO8uksmHZxfcIoq4ihu87gTH8qa2BwAVAL60kYB0Yk%2BKSeZ0C4Rr3%2BMKLqEtj2XUAtrnkhfQQ%3D"
+g_key = "<your key goes here"
